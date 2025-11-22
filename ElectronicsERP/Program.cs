ee0301e2649d91ba4852715ee2cdadaf7ad00fee
Mohsin Ali <mohsinadspec@gmail.com>
2025-11-22 13:47:26 +0500

    API changes

--- ElectronicsERP/Program.cs
+++ ElectronicsERP/Program.cs
@@ -14,6 +14,7 @@ namespace ElectronicsERP
             var builder = WebApplication.CreateBuilder(args);
 
             // ✅ Database connection
+            // Mohsin Update
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
